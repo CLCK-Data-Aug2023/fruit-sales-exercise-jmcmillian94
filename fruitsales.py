2be517adb15c493942b4c439487a3cd87f0eee7a
unknown <jmcmillian94@aim.com>
2023-12-16 14:27:48 -0500

    adding files

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,8 @@
 # add your code here
+import pandas as pd
+import os
+
+fruit_sales = pd.DataFrame({'Apples': [35,41], 'Bananas': [21,34]}, index=['2017 Sales', '2018 Sales'])
+
+file_path = 'fruit.csv'
+fruit_sales.to_csv(file_path)
